983bf8eb03a85dc79de93ce18530532bad38f425
tomasinderdal-star <tomasinderdal@gmail.com>
2026-09-21 18:17:55 +0200

    Bump cache for manual animal workflow

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='avlsoversikt-v18';
+const CACHE='avlsoversikt-v19';
 const FILES=['./','./index.html','./manifest.webmanifest','./icon-192.png','./icon-512.png'];
 self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(FILES)))});
 self.addEventListener('activate',e=>e.waitUntil(Promise.all([self.clients.claim(),caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))])));
